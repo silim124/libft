20ac7c02f8125188d9951871e7535be00f149d2e
sini0124 <siniim0124@gmail.com>
2020-10-11 16:33:47 +0900

    fix some bugs

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,38 +6,40 @@
 /*   By: silim <silim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/10 15:21:38 by silim             #+#    #+#             */
-/*   Updated: 2020/10/11 00:09:08 by silim            ###   ########.fr       */
+/*   Updated: 2020/10/11 16:32:59 by silim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_is_set(char const c, char const *set)
+{
+	size_t	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (c == set[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	current_index;
-	int		start_index;
+	size_t	start_index;
 	size_t	end_index;
-	int		checked;
-	char	*new_str;
 
-	current_index = 0;
-	start_index = -1;
-	end_index = 0;
 	if (!*s1 || !*set)
 		return (NULL);
-	while (s1[current_index])
-	{
-		if (!(ft_strchr(set, s1[current_index])) && start_index == -1)
-			start_index = current_index;
-		if ((ft_strchr(set, s1[current_index])) && start_index != -1)
-		{
-			end_index = current_index;
-			break ;
-		}
-		++current_index;
-	}
+	start_index = 0;
+	while (s1[start_index] && ft_is_set(s1[start_index], set))
+		start_index++;
+	end_index = ft_strlen(s1) - 1;
 	if (start_index >= end_index)
 		return (ft_strdup(""));
-	new_str = ft_substr(s1, start_index, end_index - start_index + 1);
-	return (new_str);
+	while (s1[end_index] && end_index != start_index)
+		end_index--;
+	ft_substr(s1, start_index, end_index - start_index + 1);
 }
