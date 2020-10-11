32c6254380add9eb879655c0fb8bc7cc969d840b
sini0124 <siniim0124@gmail.com>
2020-10-11 16:52:38 +0900

    fix some bugs

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: silim <silim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/10 15:21:28 by silim             #+#    #+#             */
-/*   Updated: 2020/10/11 00:10:28 by silim            ###   ########.fr       */
+/*   Updated: 2020/10/11 16:51:19 by silim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	s1_len;
-	size_t	s2_len;
 	char	*new_str;
+	size_t	i;
 
-	if (!*s1 && !*s2)
-		return (NULL);
-	else if (!*s1 || !*s2)
-		return ((*s1) ? ft_strdup(s1) : ft_strdup(s2));
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	if (!(new_str = malloc(sizeof(char) * (s1_len + s2_len + 1))))
-		return (NULL);
-	memcpy(new_str, s1, s1_len + 1);
-	strlcat(new_str, s2, s2_len + 1);
+	if (!s1 || !s2)
+		return (0);
+	if (!(new_str = malloc(sizeof(char) * (ft_strlen(s1) + \
+	 ft_strlen(s2) + 1))))
+		return (0);
+	i = 0;
+	while (*s1)
+		new_str[i++] = *s1++;
+	while (*s1)
+		new_str[i++] = *s2++;
+	new_str[i] = '\0';
 	return (new_str);
 }
