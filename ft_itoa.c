b66989ccb31c33c87da24cf77060cbf6563a31da
sini0124 <siniim0124@gmail.com>
2020-10-13 00:19:15 +0900

    fix some bugs

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,13 +6,18 @@
 /*   By: silim <silim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/12 23:51:30 by silim             #+#    #+#             */
-/*   Updated: 2020/10/13 00:11:32 by silim            ###   ########.fr       */
+/*   Updated: 2020/10/13 00:19:04 by silim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_len(long nbr)
+static long	ft_abs(long nbr)
+{
+	return ((nbr < 0) ? -nbr : nbr);
+}
+
+static size_t	ft_len(long nbr)
 {
 	int	len;
 
@@ -32,17 +37,14 @@ char	*ft_itoa(int nbr)
 	n = nbr;
 	len = ft_len(n);
 	str = (char *)malloc(sizeof(char) * len + 1);
-	str[len] = '\0';
+	str[len--] = '\0';
 	if (n < 0)
-	{
 		str[0] = '-';
-		n *= -1;
-	}
 	while (len >= 0)
-		{
-			str[len] = '0' + (n % 10);
-			n = n / 10;
-			len--;
-		}
+	{
+		str[len] = '0' + ft_abs(n % 10);
+		n = ft_abs(n / 10);
+		len--;
+	}
 	return (str);
 }
