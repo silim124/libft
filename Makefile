9ec4ba49170a579442f1d662367c142d4420a99f
sini0124 <siniim0124@gmail.com>
2020-10-13 18:27:22 +0900

    fix some bugs

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ SRC = ft_strnstr.c ft_isdigit.c ft_strlcpy.c ft_strlen.c \
 		ft_memset.c ft_strncmp.c ft_memmove.c ft_strlcat.c\
 		ft_calloc.c ft_strdup.c ft_atoi.c ft_isalnum.c\
 		ft_tolower.c ft_substr.c ft_strjoin.c ft_strtrim.c\
-		ft_split.c ft_itoa.c
+		ft_split.c ft_itoa.c ft_strmapi.c
 
 OBJ = ${SRC:.c=.o}
 
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: silim <silim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/13 17:07:47 by silim             #+#    #+#             */
-/*   Updated: 2020/10/13 17:07:56 by silim            ###   ########.fr       */
+/*   Updated: 2020/10/13 18:26:43 by silim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,16 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	char	*new_str;
+	size_t	len;
+
 	if (!s || !f)
 		return (0);
-
-
+	len = ft_strlen(s);
+	if (!(new_str = malloc(sizeof(char) + (len + 1))))
+		return (0);
+	new_str[len] = 0;
+	while (--len >= 0)
+		new_str[len] = f(len, s[len]);
+	return (new_str);
+}
